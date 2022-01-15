b6c72f82a4967da19e0de162db361374d61e05ce
Sean S. LeBlanc <sean.s.leblanc@gmail.com>
2022-01-15 18:53:49 -0500

    fix: `loaders` not defined
    BREAKING CHANGE: restricts peer dependency to pixi v6

--- index.js
+++ index.js
@@ -2,13 +2,13 @@ import {
 	Howl
 } from 'howler';
 import {
-	loaders
+	LoaderResource
 } from 'pixi.js';
 
 
 export default function HowlerMiddleware(resource, next) {
 	if (resource && ["wav", "ogg", "mp3", "mpeg"].includes(resource.extension)) {
-		resource._setFlag(loaders.Resource.STATUS_FLAGS.LOADING, true);
+		resource._setFlag(LoaderResource.STATUS_FLAGS.LOADING, true);
 		const options = JSON.parse(JSON.stringify(resource.metadata));
 		options.src = [resource.url];
 		options.onload = function () {
